994872e8e9e5a90bfef2a31eeaa5e31f0cd05212
Jason Gong <jasongong83@gmail.com>
2021-09-27 00:17:42 -0400

    set imports initial variables and constructor

--- contracts/contribution-no-oz.sol
+++ contracts/contribution-no-oz.sol
@@ -2,30 +2,26 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
 
-contract Contribution is ReentrancyGuard {
+
+contract ContributionNoOz is ReentrancyGuard {
+  using Counters for Counters.Counter;
+  Counters.Counter private _contributerCount;
+
   address owner;
-  uint private maxContribution;
   bool private canWithdraw;
+  bool private canDeposit;
+  uint private maxContribution;
+  uint private shareValue;
+  uint private contractBalance;
   mapping ( address => uint256 ) private balances;
   constructor() {
     owner = msg.sender;
-    maxContribution = 10;
     canWithdraw = false;
+    canDeposit = true;
+    maxContribution = 10;
   }
 
-  function setWithdraw(bool enableWithdraw) external {
-    require(msg.sender == owner, "only the owner may change the ability to withdraw");
-
-    canWithdraw = enableWithdraw;
-  }
-
-
-  function deposit() external payable {
-    balances[msg.sender] += msg.value;
-  }
-  function changeMaxContribution(uint newMaxContrinution) external {
-    require(msg.sender == owner, "only the owner may change the max contribution");
-    maxContribution = newMaxContrinution;
-  }
 }
