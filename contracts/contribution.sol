20a4a97200fc7ad1d43f399f91461b07800aa24b
Jason Gong <jasongong83@gmail.com>
2021-09-26 21:11:24 -0400

    add changeMaxContribution function

--- contracts/contribution.sol
+++ contracts/contribution.sol
@@ -4,7 +4,13 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Contribution is Ownable {
+  mapping ( address => uint256 ) public balances;
+  uint private maxContribution;
   constructor() {
+    maxContribution = 10;
+  }
 
+  function changeMaxContribution(uint newMaxContrinution) public onlyOwner{
+    maxContribution = newMaxContrinution;
   }
 }
