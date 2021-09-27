8304a1458daa739c48cb3e7861693cea3f4d067d
Jason Gong <jasongong83@gmail.com>
2021-09-27 00:12:28 -0400

    edit deploy script to only deploy contributionnooz

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,15 @@
+const hre = require("hardhat");
 
+async function main() {
+  const Contribute = await hre.ethers.getContractFactory("ContributionNoOz");
+  const contribute = await Contribute.deploy();
+  await contribute.deployed();
+  console.log("contribution deployed to:", contribute.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
