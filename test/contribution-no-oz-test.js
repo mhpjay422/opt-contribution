eaf5b69c497ff44301a23705464e56c28c01e326
Jason Gong <jasongong83@gmail.com>
2021-09-27 21:26:31 -0400

    add tests for depost

--- test/contribution-no-oz-test.js
+++ test/contribution-no-oz-test.js
@@ -11,6 +11,60 @@ describe("ContributionNoOz", function () {
     await this.contribution.deployed();
   });
 
+  describe("deposit", function () {
+    it("allows a user to make a deposit", async function () {
+      [owner] = await ethers.getSigners();
+      contractAddress = this.contribution.address;
+      await this.contribution.connect(owner).deposit({
+        value: 10,
+      });
+
+      expect(await this.contribution.getContractBalance()).to.equal(10);
+    });
+
+    it("disallows a user to make a deposit when greater than the maximum allowed", async function () {
+      [owner] = await ethers.getSigners();
+      contractAddress = this.contribution.address;
+
+      await expect(
+        this.contribution.connect(owner).deposit({
+          value: 11,
+        })
+      ).to.be.revertedWith("your balance is greater than the allowed amount");
+    });
+
+    it("disallows a user to make a deposit when greater than the maximum allowed after there is already a balance in the account", async function () {
+      [owner] = await ethers.getSigners();
+      contractAddress = this.contribution.address;
+      await this.contribution.connect(owner).deposit({
+        value: 10,
+      });
+
+      await expect(
+        this.contribution.connect(owner).deposit({
+          value: 1,
+        })
+      ).to.be.revertedWith("your balance is greater than the allowed amount");
+    });
+
+    it("disallows a user to make a deposit when canDeposit is set to false", async function () {
+      [owner] = await ethers.getSigners();
+      contractAddress = this.contribution.address;
+      await this.contribution.connect(owner).deposit({
+        value: 1,
+      });
+      await this.contribution
+        .connect(owner)
+        .closeContributionAndEnableWithdraw();
+
+      await expect(
+        this.contribution.connect(owner).deposit({
+          value: 1,
+        })
+      ).to.be.revertedWith("Deposit has been disabled");
+    });
+  });
+
   describe("changeMaxContribution", function () {
     it("changes the max contribution", async function () {
       [owner] = await ethers.getSigners();
@@ -33,7 +87,7 @@ describe("ContributionNoOz", function () {
 
   describe("getContractBalance", function () {
     it("gets the balance of the contract", async function () {
-      getBalance = await this.contribution.contractBalance();
+      getBalance = await this.contribution.getContractBalance();
       expect(getBalance).to.equal(0);
     });
 
@@ -43,7 +97,7 @@ describe("ContributionNoOz", function () {
       await this.contribution.connect(user).deposit({
         value: 10,
       });
-      getBalance = await this.contribution.contractBalance();
+      getBalance = await this.contribution.getContractBalance();
 
       expect(getBalance).to.equal(10);
     });
